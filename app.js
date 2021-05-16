112322e5e5a6a042336cb80b81325041901d5c43
genadi53 <kotka5353@gmail.com>
2021-05-16 09:58:35 +0300

    feat: install hellmet

--- app.js
+++ app.js
@@ -9,15 +9,68 @@ const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const MongoStore = require('connect-mongo');
+
 const AppError = require('./utils/AppErrors');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-const passport = require('passport');
-const localStrategy = require('passport-local');
 const User = require('./models/user');
 const userRoutes = require('./routes/users');
 
-mongoose.connect('mongodb://localhost:27017/v2-yelpCamp', {
+
+const databasebUrl = process.env.DB_URL || 'mongodb://localhost:27017/v2-yelpCamp';
+const secret = process.env.SECRET || 'verylongsecretmessage';
+const storeOptions = {
+    mongoUrl: databasebUrl,
+    secret: secret,
+    touchAfter: 24 * 3600,
+    crypto: {
+        secret: secret
+    },
+};
+const sessionConfig = {
+    name: 'randomSessionName',
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() * 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+    store: MongoStore.create(storeOptions),
+};
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://kit.fontawesome.com",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com",
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.mapbox.com",
+    "https://api.tiles.mapbox.com",
+    "https://fonts.googleapis.com",
+    "https://use.fontawesome.com",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com",
+    "https://*.tiles.mapbox.com",
+    "https://events.mapbox.com",
+];
+const fontSrcUrls = [];
+
+
+
+mongoose.connect(databasebUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -38,17 +91,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-
-const sessionConfig = {
-    secret: 'longsecretmessage',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() * 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-    }
-}
+app.use(mongoSanitize());
+app.use(helmet({contentSecurityPolicy: false}))
 app.use(session(sessionConfig))
 app.use(flash());
 app.use(passport.initialize());
@@ -58,9 +102,29 @@ passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/ddm9sjjq5/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com",
+                "https://images.wunderstock.com",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
@@ -69,15 +133,13 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.get('/fakeuser', async (req, res, next) => {
-//     const user = new User({ email: 'newmail@gmail.com', username: 'colttt' })
-//     const newUser = await User.register(user, 'qwerty');
-//     res.send(newUser);
-// })
-
 app.use('/', userRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes)
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 
 app.all('*', (req, res, next) => {
